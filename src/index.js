f95dbd86275ba5ab1fa14855efeae1d695990fe0
Balicea28 <b.alicea1998@gmail.com>
2022-09-08 10:31:38 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = 
+  document.getElementById('create-task-form');
+  form.addEventListener, ('submit' e => {
+    e.preventDefault()
+    buildTodo(e.target['new-task-description'].value)
+    form.reset()
+  }
 });
